b505ad4627501f6f724494c65c6427275c5943e7
marvin <marvinma.mw@gmail.com>
2021-04-06 18:25:54 -0400

    project2

---
+++ Employees Project/Employee.js
@@ -0,0 +1,12 @@
+
+class Employee{
+    constructor(name,title,salary,boss){
+
+    }
+}
+
+module.export.Employee
+
+//  module.export={
+//     Employee:Employee
+// }
---
+++ Employees Project/Manager.js
@@ -0,0 +1,8 @@
+const {Employee}=reuire('./Manager')
+
+class Manager extends Employee{
+    constructor(name,title,salary,boss,arr){
+        super(name,title,salary,boss)
+    }
+
+}
