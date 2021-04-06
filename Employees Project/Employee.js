a570af5a24e6a2172f58e39b0e40378dafb77fba
Andru <dr3wwatkins@gmail.com>
2021-04-06 17:44:45 -0500

    Added functions for Employee and Manager

--- Employees Project/Employee.js
+++ Employees Project/Employee.js
@@ -1,7 +1,13 @@
 
 class Employee{
     constructor(name,title,salary,boss){
-
+        this.name = name;
+        this.title = title;
+        this.salary = salary;
+        this.boss = boss;
+    }
+    bonus(multiplier) {
+        return this.salary * multiplier;
     }
 }
 
--- Employees Project/Manager.js
+++ Employees Project/Manager.js
@@ -1,8 +1,11 @@
-const {Employee}=reuire('./Manager')
+const {Employee} = require('./Manager')
 
 class Manager extends Employee{
     constructor(name,title,salary,boss,arr){
         super(name,title,salary,boss)
+        this.arr = arr;
     }
-
+    bonus(multiplier) {
+        return
+    }
 }
